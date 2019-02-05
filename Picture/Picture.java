8963c8be3b8b5adfb1d05ca5bfd22dd5accdd481
Swathi Bikumalia <swathibikumalia@zipcoders-MacBook-Pro.local>
2019-02-04 20:49:21 -0500

    Added windows

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -13,7 +13,10 @@
 public class Picture
 {
     private Square wall;
-    private Square window;
+    private Square door1;
+    private Square window1;
+    private Square window2;
+    private Square door2;
     private Triangle roof;
     private Circle sun;
 
@@ -31,26 +34,53 @@ public class Picture
     public void draw()
     {
         wall = new Square();
-        wall.moveVertical(80);
-        wall.changeSize(100);
+        wall.moveVertical(45);
+        wall.changeSize(150);
         wall.makeVisible();
+        
 
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
-        window.makeVisible();
+        window1 = new Square();
+        window1.changeColor("black");
+        window1.moveHorizontal(20);
+        window1.moveVertical(100);
+        window1.changeSize(25);
+        window1.makeVisible();
+        
+        window2 = new Square();
+        window2.changeColor("black");
+        window2.moveHorizontal(105);
+        window2.moveVertical(100);
+        window2.changeSize(25);
+        window2.makeVisible();
+        
+        
+        door1 = new Square();
+        door1.changeColor("white");
+        door1.moveHorizontal(60);
+        door1.moveVertical(165);
+        door1.changeSize(30);
+        door1.makeVisible();
+        
+        door2 = new Square();
+        door2.changeColor("white");
+        door2.moveHorizontal(60);
+        door2.moveVertical(145);
+        door2.changeSize(30);
+        door2.makeVisible();
+        
+        
+       
 
         roof = new Triangle();
-        roof.changeSize(50, 140);
-        roof.moveHorizontal(60);
-        roof.moveVertical(70);
+        roof.changeSize(150, 150);
+        roof.moveHorizontal(100);
+        roof.moveVertical(-20);
         roof.makeVisible();
 
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
+        sun.moveHorizontal(220);
+        sun.moveVertical(-25);
         sun.changeSize(60);
         sun.makeVisible();
     }
@@ -63,7 +93,7 @@ public class Picture
         if(wall != null)   // only if it's painted already...
         {
             wall.changeColor("black");
-            window.changeColor("white");
+            window1.changeColor("white");
             roof.changeColor("black");
             sun.changeColor("black");
         }
@@ -77,7 +107,7 @@ public class Picture
         if(wall != null)   // only if it's painted already...
         {
             wall.changeColor("red");
-            window.changeColor("black");
+            window1.changeColor("black");
             roof.changeColor("green");
             sun.changeColor("yellow");
         }
